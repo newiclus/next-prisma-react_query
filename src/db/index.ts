f0ad8697e7bfbc915346bb8bdfce182115d22acb
Junihor Moran <newiclus2005@gmail.com>
2022-06-20 15:57:36 -0500

    fix: PrismaClient singlenton

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,15 +1,29 @@
 import { PrismaClient } from '@prisma/client'
 
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
+
 declare global {
   // allow global `var` declarations
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined
 }
 
-export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ['query'],
-  })
+let prisma: PrismaClient
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+
+  prisma = global.prisma
+}
+
+export default prisma
